90a0ffd9f3793e739c55ed841d4949d22ba3ea06
Harshil Ganapathi <harshilganapathi@Harshils-MacBook-Air.local>
2022-08-07 10:49:54 -0400

    I did something

--- backend.py
+++ backend.py
@@ -1,4 +1,10 @@
 import sqlite3
+from googleapiclient import discovery
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+import pickle 
+from datetime import datetime, timedelta
+from testrun import *
 
 # connect to database
 conn = None
@@ -7,7 +13,9 @@ cursor = conn.cursor()
 
 rows = cursor.execute("SELECT id, title, complete FROM todo").fetchall()
 print(rows)
-
+for x in rows:
+    print(x[1])
+    create_calendar(x[1])
 
 
 
