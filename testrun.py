96e188fee5db366037d4bd8ddc1ab9812fa74352
Harshil Ganapathi <harshilganapathi@Harshils-MacBook-Air.local>
2022-08-07 00:30:52 -0400

    added backend google calendar

---
+++ testrun.py
@@ -0,0 +1,45 @@
+from googleapiclient import discovery
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+import pickle 
+from datetime import datetime, timedelta
+
+ 
+scopes = ['https://www.googleapis.com/auth/calendar']
+flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes=scopes)
+
+credentials = flow.run_console()
+
+pickle.dump(credentials, open("token.pkl", "wb"))
+credentials = pickle.load(open("token.pkl", "rb"))
+service = build("calendar", "v3", credentials=credentials)
+
+result = service.calendarList().list().execute()
+
+start_time = datetime(2022, 8, 7, 21, 0, 0)
+end_time = start_time + timedelta(hours=2)
+
+timezone = 'America/New_York'
+
+event = {
+  'summary': 'Google I/O 2015',
+  'location': '800 Howard St., San Francisco, CA 94103',
+  'description': 'A chance to hear more about Google\'s developer products.',
+  'start': {
+    'dateTime': start_time.strftime("%Y-%m-%dT%H:%M:%S"),
+    'timeZone': timezone,
+  },
+  'end': {
+    'dateTime': end_time.strftime("%Y-%m-%dT%H:%M:%S"),
+    'timeZone': timezone,
+  },
+  'reminders': {
+    'useDefault': False,
+    'overrides': [
+      {'method': 'email', 'minutes': 24 * 60},
+      {'method': 'popup', 'minutes': 10},
+    ],
+  },
+}
+
+event = service.events().insert(calendarId='primary', body=event).execute()
